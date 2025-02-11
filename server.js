363db8c5b8f7a302f395fe792ea6fd98f9a74d53
Akshaye <65844360+akshayedharshak@users.noreply.github.com>
2025-02-11 13:17:07 +0000

    VM-2

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const app = express()
+const express = require('express')
+const app = express();
 
-app.get('/users', (req , res) => {
-res.json([{id:1, name: 'Alice'} ,{id:2 , name: 'Bob'}]);
+app.get('/orders', (req , res)=> {
+res.json([{orderId: 101, userId: 1, amount: 500}]);
 });
-
-app.listen(3000 , () => console.log('User Service running on port 3000'));
+app.listen(4000 ,() => console.log('Order Service running on port 4000'));
